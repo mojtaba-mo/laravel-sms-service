46e8cc0060f73bf79ee97f8460cab14d48763076
mojtaba <160061648+mojtaba-mo@users.noreply.github.com>
2025-09-21 21:30:36 +0330

    Update sms.php

--- config/sms.php
+++ config/sms.php
@@ -4,12 +4,12 @@ return [
     'melipayamak' => [
         'username' => env('MELIPAYAMAK_USERNAME'),
         'password' => env('MELIPAYAMAK_PASSWORD'),
+        'bodyId' => env('OTP_BODY_ID', 1234),
         'endpoint' => env('MELIPAYAMAK_ENDPOINT', 'http://api.payamak-panel.com/post/Send.asmx?wsdl')
     ],
     
     // OTP Settings
     'ttl_minutes' => env('OTP_TTL_MINUTES', 2),
     'daily_limit' => env('OTP_DAILY_LIMIT', 3),
-    'min_interval_seconds' => env('OTP_MIN_INTERVAL_SECONDS', 120),
-    'bodyId' => env('OTP_BODY_ID', 1234)
+    'min_interval_seconds' => env('OTP_MIN_INTERVAL_SECONDS', 120)
 ];
